6e7ed0eb493c4f80fb6efde54a20d3b450d25813
Ben Ovsiew <42625395+bovsiew@users.noreply.github.com>
2018-08-28 13:05:32 -0400

    Added python file

---
+++ Test.py
@@ -0,0 +1,18 @@
+x, y = 27, 5
+
+if x %2 == 0:
+    print("Divisible by 2")
+else:
+    print("Not divisible")
+
+# more conditionals
+if x % 2 == 0:
+    if x % 3 == 0:
+        print("Divisible by 2 and 3")
+    else:
+        print("Divisible by 2 but not 3")
+else:
+    print("Divisible by 3 and not 2")
+
+
+
